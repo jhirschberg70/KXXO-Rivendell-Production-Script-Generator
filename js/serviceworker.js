93ce5b7c5085f70174521f46ef633ad08234d717
jhirschberg70 <48007413+jhirschberg70@users.noreply.github.com>
2024-04-21 08:26:48 -0700

    Make tab creation and message selection more robust. While the previous version worked, I believe that I was technically getting lucky that the onMessage handler in rivendell.js was ready to receive a message from serviceworker.js.  This new approach ensures that rivendell.html and rivendell.js have loaded before serviceworker.js attempts to send the message.

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -1,14 +1,36 @@
-// Event listener for when the extension icon is clicked
 chrome.action.onClicked.addListener(async (tab) => {
-  const productionTab = await chrome.tabs.create({ url: "../rivendell.html" });
+  const message = await createMessage(tab);
+  const productionTab = await createTab("../rivendell.html");
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: () => {
-      return getSelection().toString();
-    }
-  }).then((selectedText) => {
-    const msg = selectedText[0].result;
-    chrome.tabs.sendMessage(productionTab.id, msg);
-  });
+  chrome.tabs.sendMessage(productionTab.id, message);
 });
+
+async function createMessage(tab) {
+  return new Promise(async (resolve, reject) => {
+    const selectedText = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: () => {
+        return getSelection().toString();
+      }
+    });
+    
+    resolve(selectedText[0].result);
+  });
+}
+
+async function createTab(url) {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.create({ url: url }, (tab) => {
+      if (tab.status === "complete") {
+        resolve(tab);
+      } else {
+        chrome.tabs.onUpdated.addListener(updateListener = (tabId, changeInfo) => {
+          if ((tab.id === tabId) && (changeInfo.status === "complete")) {
+            chrome.tabs.onUpdated.removeListener(updateListener);
+            resolve(tab);
+          }
+        });
+      }
+    });
+  });
+}
